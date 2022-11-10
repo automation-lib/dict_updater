6c5705577198affd62e29179f4b66a7a0af38252
chetan <chetankolhe72@gmail.com>
2022-11-10 20:34:31 +0530

    Add licence and package convetion

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'avi'
+LONG_DESCRIPTION = 'Recursive Dict Update with insert , deleter , update option'
+
+# Setting up
+setup(
+    name="dict_update",
+    version=VERSION,
+    author="Chetan",
+    author_email="chetankolhe72@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['dict updater', 'json update', 'recursive dict updater', 'recursive json updater', 'dict updater','nested dicts'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    fullname="Chetan Kolhe",
+    url="https://github.com/automation-lib/dict_updater"
+)
